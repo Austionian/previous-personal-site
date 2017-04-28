12fd04f55c03905dbb62f369af778c12d6e5e2bc
austionian <arooks@hawaii.edu>
2017-04-28 15:52:10 -0500

    fix copywrite date

--- app/js/Footer.js
+++ app/js/Footer.js
@@ -2,4 +2,4 @@ getYear = () => new Date(Date.now()).getFullYear()
 
 document.addEventListener('DOMContentLoaded', function(event) {
   document.getElementById('year').innerHTML = getYear();
-})
+});
